79e829b3df50e984e33797d46c0af41081cee2d5
tac0trucks <abainciajoelconstantine@gmail.com>
2026-05-10 17:20:18 +0800

    Fixed Login

--- db.php
+++ db.php
@@ -1,15 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root"; 
-$password = "admin";     
+$username = "root";
+$password = "admin"; // Make sure this is your Workbench password!
 $dbname = "cit_university_db";
 
-// Create connection
+// THIS IS THE IMPORTANT LINE:
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-// If you see nothing when loading this, the connection was successful!
 ?>
